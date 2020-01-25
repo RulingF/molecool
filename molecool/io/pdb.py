f4b9e9a6c25f24563d9de8391bba9fb24b48c790
Rulin Feng <student.frel@gmail.com>
2020-01-25 15:20:54 -0500

    Add more docstrings.

--- molecool/io/pdb.py
+++ molecool/io/pdb.py
@@ -6,6 +6,30 @@ This module handles pdb type of data.
 import numpy as py
 
 def open_pdb(file_location):
+    """
+    Open and read coordinates and symbols from a pbd file.
+
+    The pbd file must specify the atom elements in the last column, and follow the conventions outlined in the PDB format specification.
+
+    Parameters
+    ----------
+    file_location : str
+        The directory of the file.
+
+    Returns
+    -------
+    symbols : str
+        The name of the element.
+
+    coordinates : np.ndarray
+        3D list containing the xyz coordinates of the atom.
+
+    Examples
+    --------
+    >>> file_location = ""
+    >>> open_pdb(file_location)
+    >>> 
+    """
     # This function reads in a pdb file and returns the atom names and coordinates.
     with open(file_location) as f:
         data = f.readlines()
